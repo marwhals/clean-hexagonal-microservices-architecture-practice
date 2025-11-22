66f8f375a5a0cc5cf16c026180c4fa6d90bde51d
marwhals <marjan.mubarok2012@gmail.com>
2025-11-22 23:00:36 +0000

    Implement Order Domain Service

--- order-service/order-domain/order-domain-core/src/main/java/core/domain/entity/Order.java
+++ order-service/order-domain/order-domain-core/src/main/java/core/domain/entity/Order.java
@@ -17,7 +17,7 @@ public class Order extends AggregateRoot<OrderId> {
     private OrderStatus orderStatus;
     private List<String> failureMessages;
 
-    public void initializerOrder() {
+    public void initializeOrder() {
         setId(new OrderId(UUID.randomUUID()));
         trackingId = new TrackingId(UUID.randomUUID());
         orderStatus = OrderStatus.PENDING;
