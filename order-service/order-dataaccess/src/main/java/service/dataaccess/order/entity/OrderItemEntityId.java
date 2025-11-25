39154a5f8ca490df2dde1f1e0e1400663bb5ea36
marwhals <marjan.mubarok2012@gmail.com>
2025-11-25 16:07:49 +0000

    Add order JPA Entity to data access module

---
+++ order-service/order-dataaccess/src/main/java/service/dataaccess/order/entity/OrderItemEntityId.java
@@ -0,0 +1,29 @@
+package service.dataaccess.order.entity;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemEntityId implements Serializable {
+
+    private Long id;
+    private OrderEntity order;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemEntityId that = (OrderItemEntityId) o;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getOrder(), that.getOrder());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getOrder());
+    }
+}
