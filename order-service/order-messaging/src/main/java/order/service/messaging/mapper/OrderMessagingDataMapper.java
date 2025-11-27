c56c16f2f9f98e7493dabd8bfefee9abb92be428
marwhals <marjan.mubarok2012@gmail.com>
2025-11-27 17:09:19 +0000

    Add Kafka Avro mapper and config

---
+++ order-service/order-messaging/src/main/java/order/service/messaging/mapper/OrderMessagingDataMapper.java
@@ -0,0 +1,41 @@
+package order.service.messaging.mapper;
+
+import core.domain.entity.Order;
+import core.domain.event.OrderCancelledEvent;
+import core.domain.event.OrderCreatedEvent;
+import kafka.order.avro.model.PaymentOrderStatus;
+import kafka.order.avro.model.PaymentRequestAvroModel;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class OrderMessagingDataMapper {
+
+    public PaymentRequestAvroModel orderCreatedEventToPaymentRequestAvroModel(OrderCreatedEvent orderCreatedEvent) {
+        Order order = orderCreatedEvent.getOrder();
+        return PaymentRequestAvroModel.newBuilder()
+                .setId(UUID.randomUUID().toString())
+                .setSagaId("")
+                .setCustomerId(order.getCustomerId().getValue().toString())
+                .setOrderId(order.getId().getValue().toString())
+                .setPrice(order.getPrice().getAmount())
+                .setCreatedAt(orderCreatedEvent.getCreatedAt().toInstant())
+                .setPaymentOrderStatus(PaymentOrderStatus.PENDING)
+                .build();
+    }
+
+    public PaymentRequestAvroModel orderCancelledEventToPaymentRequestAvroModel(OrderCancelledEvent orderCancelledEvent) {
+        Order order = orderCancelledEvent.getOrder();
+        return PaymentRequestAvroModel.newBuilder()
+                .setId(UUID.randomUUID().toString())
+                .setSagaId("")
+                .setCustomerId(order.getCustomerId().getValue().toString())
+                .setOrderId(order.getId().getValue().toString())
+                .setPrice(order.getPrice().getAmount())
+                .setCreatedAt(orderCancelledEvent.getCreatedAt().toInstant())
+                .setPaymentOrderStatus(PaymentOrderStatus.CANCELLED)
+                .build();
+    }
+    
+}
