a5a815b15704caea076a08e6fff4c48441354ab6
marwhals <marjan.mubarok2012@gmail.com>
2025-11-30 20:37:34 +0000

    Add messaging module

---
+++ restaurant-service/restaurant-messaging/src/main/java/restaurant/service/messaging/mapper/RestaurantMessagingDataMapper.java
@@ -0,0 +1,68 @@
+package restaurant.service.messaging.mapper;
+
+import core.domain.valueobject.ProductId;
+import core.domain.valueobject.RestaurantOrderStatus;
+import kafka.order.avro.model.OrderApprovalStatus;
+import kafka.order.avro.model.RestaurantApprovalRequestAvroModel;
+import kafka.order.avro.model.RestaurantApprovalResponseAvroModel;
+import org.springframework.stereotype.Component;
+import payment.service.domain.dto.RestaurantApprovalRequest;
+import restaurant.service.domain.entity.Product;
+import restaurant.service.domain.event.OrderApprovedEvent;
+import restaurant.service.domain.event.OrderRejectedEvent;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class RestaurantMessagingDataMapper {
+    public RestaurantApprovalResponseAvroModel
+    orderApprovedEventToRestaurantApprovalResponseAvroModel(OrderApprovedEvent orderApprovedEvent) {
+        return RestaurantApprovalResponseAvroModel.newBuilder()
+                .setId(UUID.randomUUID().toString())
+                .setSagaId("")
+                .setOrderId(orderApprovedEvent.getOrderApproval().getOrderId().getValue().toString())
+                .setRestaurantId(orderApprovedEvent.getRestaurantId().getValue().toString())
+                .setCreatedAt(orderApprovedEvent.getCreatedAt().toInstant())
+                .setOrderApprovalStatus(OrderApprovalStatus.valueOf(orderApprovedEvent.
+                        getOrderApproval().getApprovalStatus().name()))
+                .setFailureMessages(orderApprovedEvent.getFailureMessages())
+                .build();
+    }
+
+    public RestaurantApprovalResponseAvroModel
+    orderRejectedEventToRestaurantApprovalResponseAvroModel(OrderRejectedEvent orderRejectedEvent) {
+        return RestaurantApprovalResponseAvroModel.newBuilder()
+                .setId(UUID.randomUUID().toString())
+                .setSagaId("")
+                .setOrderId(orderRejectedEvent.getOrderApproval().getOrderId().getValue().toString())
+                .setRestaurantId(orderRejectedEvent.getRestaurantId().getValue().toString())
+                .setCreatedAt(orderRejectedEvent.getCreatedAt().toInstant())
+                .setOrderApprovalStatus(OrderApprovalStatus.valueOf(orderRejectedEvent.
+                        getOrderApproval().getApprovalStatus().name()))
+                .setFailureMessages(orderRejectedEvent.getFailureMessages())
+                .build();
+    }
+
+    public RestaurantApprovalRequest
+    restaurantApprovalRequestAvroModelToRestaurantApproval(RestaurantApprovalRequestAvroModel
+                                                                   restaurantApprovalRequestAvroModel) {
+        return RestaurantApprovalRequest.builder()
+                .id(restaurantApprovalRequestAvroModel.getId())
+                .sagaId(restaurantApprovalRequestAvroModel.getSagaId())
+                .restaurantId(restaurantApprovalRequestAvroModel.getRestaurantId())
+                .orderId(restaurantApprovalRequestAvroModel.getOrderId())
+                .restaurantOrderStatus(RestaurantOrderStatus.valueOf(restaurantApprovalRequestAvroModel
+                        .getRestaurantOrderStatus().name()))
+                .products(restaurantApprovalRequestAvroModel.getProducts()
+                        .stream().map(avroModel ->
+                                Product.builder()
+                                        .productId(new ProductId(UUID.fromString(avroModel.getId())))
+                                        .quantity(avroModel.getQuantity())
+                                        .build())
+                        .collect(Collectors.toList()))
+                .price(restaurantApprovalRequestAvroModel.getPrice())
+                .createdAt(restaurantApprovalRequestAvroModel.getCreatedAt())
+                .build();
+    }
+}
